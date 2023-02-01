23fb96aa64a599574149fbefe2ad5cc10f4187b3
John Abashidze <john.abashidze@gmail.com>
2023-02-01 20:18:37 +0400

    Boilerplate project

---
+++ README.md
@@ -0,0 +1,9 @@
+## Tic Tac Toe
+
+### Welcome 👋
+
+Thanks for checking out this front-end coding challenge.
+
+### Preview
+
+<img src="./preview/preview.jpg"/>
---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      {/* <!-- New game menu start --> */}
+
+      Pick player 1's mark
+      Remember: X goes first
+
+      New Game (vs CPU)
+      New Game (vs player)
+
+      {/* <!-- New game menu end --> */}
+
+      {/* <!-- Game board start --> */}
+
+      {/* <!-- x/o icon --> */} turn
+
+      X (You) {/*  <!-- Your score --> */}
+      Ties {/*  <!-- Ties score --> */}
+      X (CPU) {/* <!-- CPU score --> */}
+
+      Oh no, you lost
+      You won!
+      Player {/* <!-- 1/2 -->*/} wins!
+
+      {/*<!-- x/o icon -->*/} takes the round
+      Round tied
+      Restart game?
+
+      Quit
+      Next round
+      No, cancel
+      Yes, restart
+
+      {/*<!-- Game board end -->*/}
+    </div>
+  );
+}
+
+export default App;
