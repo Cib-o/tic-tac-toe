538a0f7564608a797f6e0bf83c5953fc51556f63
John Abashidze <john.abashidze@gmail.com>
2023-02-07 16:36:58 +0400

    Created new file GameStart.jsx in components folder and changed App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,29 +5,6 @@ function App() {
   return (
     <div>
       <GameMenu />
-
-      {/* <!-- Game board start --> */}
-
-      {/* <!-- x/o icon --> */} turn
-
-      X (You) {/*  <!-- Your score --> */}
-      Ties {/*  <!-- Ties score --> */}
-      X (CPU) {/* <!-- CPU score --> */}
-
-      Oh no, you lost
-      You won!
-      Player {/* <!-- 1/2 -->*/} wins!
-
-      {/*<!-- x/o icon -->*/} takes the round
-      Round tied
-      Restart game?
-
-      Quit
-      Next round
-      No, cancel
-      Yes, restart
-
-      {/*<!-- Game board end -->*/}
     </div>
   );
 }
---
+++ src/components/GameStart.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function GameStart() {
+  return (
+    <div>
+      {/* <!-- Game board start --> */}
+
+      {/* <!-- x/o icon --> */} turn
+
+      X (You) {/*  <!-- Your score --> */}
+      Ties {/*  <!-- Ties score --> */}
+      X (CPU) {/* <!-- CPU score --> */}
+
+      Oh no, you lost
+      You won!
+      Player {/* <!-- 1/2 -->*/} wins!
+
+      {/*<!-- x/o icon -->*/} takes the round
+      Round tied
+      Restart game?
+
+      Quit
+      Next round
+      No, cancel
+      Yes, restart
+
+      {/*<!-- Game board end -->*/}
+    </div>
+  );
+}
+
+export default GameStart;
