c61885502720291225195ea3660e35d9111624d5
John Abashidze <john.abashidze@gmail.com>
2023-02-07 16:25:49 +0400

    Imported GameMenu component in the App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
+import GameMenu from "./GameMenu";
 
 function App() {
   return (
     <div>
-      {/* <!-- New game menu start --> */}
-
-      Pick player 1's mark
-      Remember: X goes first
-
-      New Game (vs CPU)
-      New Game (vs player)
-
-      {/* <!-- New game menu end --> */}
+      <GameMenu />
 
       {/* <!-- Game board start --> */}
 
