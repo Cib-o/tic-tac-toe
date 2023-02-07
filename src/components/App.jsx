20300205920d26b6cfcecb2d093634b231872683
John Abashidze <john.abashidze@gmail.com>
2023-02-07 17:07:58 +0400

    Removed extra div element from App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import GameMenu from "./GameMenu";
 
 function App() {
-  return (
-    <div>
-      <GameMenu />
-    </div>
-  );
+  return <GameMenu />;
 }
 
 export default App;
