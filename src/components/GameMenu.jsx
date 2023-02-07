6b80579fe5d1dce07c35c469f759709615c59902
John Abashidze <john.abashidze@gmail.com>
2023-02-07 16:22:18 +0400

    Created new file GameMenu.jsx in the components folder

---
+++ src/components/GameMenu.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function GameMenu() {
+  return (
+    <div>
+      {/* <!-- New game menu start --> */}
+
+      Pick player 1's mark
+      Remember: X goes first
+
+      New Game (vs CPU)
+      New Game (vs player)
+
+      {/* <!-- New game menu end --> */}
+    </div>
+  );
+}
+
+export default GameMenu;
