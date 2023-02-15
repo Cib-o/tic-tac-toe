4935289fdd8b2d9eca84e9ad1d3ddcc74446e147
John Abashidze <john.abashidze@gmail.com>
2023-02-15 18:03:54 +0400

    set gameMode value to false

--- src/components/GameMenu.jsx
+++ src/components/GameMenu.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function GameMenu(props) {
   const [gameMenuData, setGameMenuData] = useState({
-    gameMode: "",
+    gameMode: false,
     mark: "O",
   });
 
