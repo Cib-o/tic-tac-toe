019d1217b554b725429fbacdd971e631317f2e4f
John Abashidze <john.abashidze@gmail.com>
2023-02-18 17:47:11 +0400

    Add new feature
    Store data from props using const, also check player's mark and gameMode

--- src/components/GameStart.jsx
+++ src/components/GameStart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 function GameStart(props) {
+  const { gameMode, mark } = props.data;
+
   return (
     <div className="game-start">
       <div className="top-section">
@@ -9,7 +11,9 @@ function GameStart(props) {
           <img src="./assets/icon-x-unselected.svg" alt="icon-x" />
           <h4>Turn</h4>
         </div>
-        <button className="restart-button"><img src="./assets/icon-restart.svg" alt="icon-restart" /></button>
+        <button className="restart-button">
+          <img src="./assets/icon-restart.svg" alt="icon-restart" />
+        </button>
       </div>
 
       <div className="middle-section">
@@ -26,7 +30,7 @@ function GameStart(props) {
 
       <div className="bottom-section">
         <div className="x-score">
-          <p>X (You)</p>
+          <p>X {gameMode === "CPU" ? mark === "X" ? "(You)" : "(CPU)" : mark === "X" ? "(P1)" : "(P2)"}</p>
           <h2>0</h2>
         </div>
 
@@ -36,7 +40,7 @@ function GameStart(props) {
         </div>
 
         <div className="o-score">
-          <p>O (CPU)</p>
+          <p>O {gameMode === "CPU" ? mark === "O" ? "(P1)" : "(CPU)" : mark === "O" ? "(P1)" : "(P2)"}</p>
           <h2>0</h2>
         </div>
       </div>
